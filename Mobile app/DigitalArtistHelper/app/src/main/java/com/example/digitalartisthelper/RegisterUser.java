635e904f8370181777643e4c51e853ebae559646
TomaTheGreat <t.balikoev@gmail.com>
2021-05-25 15:37:54 +0700

    realtimeDatabase

--- Mobile app/DigitalArtistHelper/app/src/main/java/com/example/digitalartisthelper/RegisterUser.java
+++ Mobile app/DigitalArtistHelper/app/src/main/java/com/example/digitalartisthelper/RegisterUser.java
@@ -1,5 +1,6 @@
 package com.example.digitalartisthelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,8 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
@@ -80,6 +86,28 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
             editTextPassword.requestFocus();
             return;
         }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull  Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            User user = new User(fullname,age,email);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull  Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(RegisterUser.this,"User has been registered successfully!!!",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+
+                    }
+                });
     }
 }
 
