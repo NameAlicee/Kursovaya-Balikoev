e9121d7a7c9cebab506cee7f0274ab62ec31af55
TomaTheGreat <t.balikoev@gmail.com>
2021-05-26 12:21:39 +0700

    all screens set need last core feature

--- Mobile app/DigitalArtistHelper/app/src/main/java/com/example/digitalartisthelper/Lections.java
+++ Mobile app/DigitalArtistHelper/app/src/main/java/com/example/digitalartisthelper/Lections.java
@@ -2,13 +2,41 @@ package com.example.digitalartisthelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Lections extends AppCompatActivity {
+public class Lections extends AppCompatActivity implements View.OnClickListener {
+
+    private Button Home;
+    private Button Tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lections);
+
+        Home = (Button) findViewById(R.id.HomeButtonL);
+        Home.setOnClickListener(this);
+
+        Tasks = (Button) findViewById(R.id.TaskButtonL);
+        Tasks.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.HomeButtonL:
+                startActivity(new Intent(this,UserScreen.class));
+                break;
+
+            case R.id.TaskButtonL:
+                startActivity(new Intent(this, com.example.digitalartisthelper.Tasks.class));
+                break;
+        }
+
     }
 }
--- Mobile app/DigitalArtistHelper/app/src/main/java/com/example/digitalartisthelper/Tasks.java
+++ Mobile app/DigitalArtistHelper/app/src/main/java/com/example/digitalartisthelper/Tasks.java
@@ -2,13 +2,41 @@ package com.example.digitalartisthelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Tasks extends AppCompatActivity {
+public class Tasks extends AppCompatActivity implements View.OnClickListener{
+
+    private Button Lections;
+    private Button Home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+        Home = (Button) findViewById(R.id.HomeButtonT);
+        Home.setOnClickListener(this);
+
+        Lections = (Button) findViewById(R.id.LectionButtonT);
+        Lections.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.HomeButtonT:
+                startActivity(new Intent(this,UserScreen.class));
+                break;
+
+            case R.id.LectionButtonT:
+                startActivity(new Intent(this, com.example.digitalartisthelper.Lections.class));
+                break;
+        }
+
     }
 }
